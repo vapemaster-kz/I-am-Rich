f6c4d4aa1e2456f857ace4e75d166a75f92db387
vapemaster-kz <sanzhar.umbet@nu.edu.kz>
2022-02-12 12:12:43 +0600

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Sanzhar on 07.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
